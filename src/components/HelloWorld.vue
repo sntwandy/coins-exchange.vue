4aec3a376b0065c83820705f80b90844afcbb35b
Wandy Santana <sntwandy@gmail.com>
2020-11-24 10:33:32 -0400

    Init project

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <span>{{ text }}</span>
     <p>
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
@@ -90,7 +91,8 @@
 export default {
   name: "HelloWorld",
   props: {
-    msg: String
+    msg: String,
+    text: String
   }
 };
 </script>
